6e70c4260de75a492b35367fe2706d3f99d2a61f
Sorin I Costea <sorin-costea@users.noreply.github.com>
2021-03-21 02:08:05 +0100

    oops. fix the fix

--- src/main/java/com/tumblr/jumblr/types/Post.java
+++ src/main/java/com/tumblr/jumblr/types/Post.java
@@ -353,7 +353,7 @@ public abstract class Post extends Resource {
 
   public Boolean isOriginal() {
     if (reblogged_from_name != null || trail == null || trail.isEmpty()
-        || trail.get(0).getBlog().getName().compareToIgnoreCase(blog_name) != 0) {
+        || trail.get(0).getBlog().getName().compareToIgnoreCase(blog_name) == 0) {
       return true;
     }
     return false;
